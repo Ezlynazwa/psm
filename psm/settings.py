bf55d744dc5e21d9df4855df6c7872f87c2e4e3a
Ezlynazwa <ezlyn0910@gmail.com>
2025-06-22 10:19:32 +0800

    yt7t6

--- psm/settings.py
+++ psm/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
+            'connect_timeout': 30,
         }
     }
 }
